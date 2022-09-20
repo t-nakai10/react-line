acb33d8737b7afc78e40acbae665f42375c53509
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-20 22:50:01 +0900

    グーグル認証を使用できるようにする.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,16 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { db, auth, provider } from "firebase.js";
+import { signInWithPopup } from "firebase/auth";
 
 function SignIn() {
+  function singInWithGoogle() {
+    signInWithPopup(auth, provider);
+  }
+
   return (
     <div>
-      <Button>グーグルでログインする</Button>
+      <Button onClick={singInWithGoogle}>グーグルでログインする</Button>
     </div>
   );
 }
