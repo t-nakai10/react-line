9480437a7e8e6f8c6bcd00a6dcc8434b3ef6cfd8
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-21 22:11:17 +0900

    レンダー結果が決定された後に実行されるhooks を定義

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SignOut from "components/SignOut";
+import { db } from "firebase.js";
 
 const Line = () => {
+  const [messages, setMessages] = useState();
+  // DBの情報は最初だけでいい, 何回も読み込む必要はないためuseEffectを使用.
+  // レンダー結果が決定された後に実行される（副作用を設定できる）
+  useEffect(() => {}, []);
+
   return (
     <div>
       <SignOut />
