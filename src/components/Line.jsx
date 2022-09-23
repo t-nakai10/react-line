ef5b179489f4a28783afa963e5874620a70fc99b
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-23 22:11:37 +0900

    公式パターンじゃない方, map 使いたい時 - querySnapshot.docs と階層が異なることに注意

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from "react";
 import SignOut from "components/SignOut";
 import { db } from "firebase.js";
-import { collection } from "firebase/firestore";
+import { collection, getDocs, limit, query } from "firebase/firestore";
 
 const Line = () => {
   const [messages, setMessages] = useState();
   // DBの情報は最初だけでいい, 何回も読み込む必要はないためuseEffectを使用.
   // レンダー結果が決定された後に実行される（副作用を設定できる）
   useEffect(() => {
-    // DB の messages テーブル（コレクション）に接続.
-    // 下記は Firebase v9 では動作しない.
-    // db.collection("messages")
-    //   .orderBy("createdAt")
-    //   .limit(50)
-    //   // onSnapshot で実際に取り出し.
-    //   .onSnapshot((snapshot) => {
-    //     setMessages(snapshot.docs.map((doc) => doc.data()));
+    // クエリの作成.
+    const q = query(collection(db, "messages"), limit(50));
+    // const querySnapshot = async () => {
+    //   const docs = await getDocs(q);
+    //   docs.forEach((doc) => {
+    //     console.log(doc.data());
     //   });
+    // };
+    // querySnapshot();
+    // 実行.
+    // map 使いたいとき.
+    getDocs(q).then((querySnapshot) => {
+      setMessages(querySnapshot.docs.map((doc) => doc.data()));
+    });
 
-    // v9 対応していく.
-    setMessages(collection(db, "messages").orderBy("createdAt"));
+    // setMessages(snapShot.docs.map((doc) => doc.data()));
   }, []);
 
   return (
