72029cd4cf70eb1d38fb18e9bbd108fd6e01d1a9
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-25 18:10:03 +0900

    input に入力された値を message に代入する

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 
 const SendMessage = () => {
+  const [message, setMessage] = useState("");
+
   return (
     <div>
       <form action="">
         <div>
-          <input type="text" placeholder="メッセージを入力してください" />
+          <input
+            type="text"
+            placeholder="メッセージを入力してください"
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </div>
       </form>
     </div>
